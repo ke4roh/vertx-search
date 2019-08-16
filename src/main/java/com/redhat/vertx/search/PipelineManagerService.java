345ef5d982b54b9505e70b28523487806cbe467f
Jason Porter <lightguard.jp@gmail.com>
2019-08-16 15:23:12 -0600

    Very basic filesystem example

--- src/main/java/com/redhat/vertx/search/PipelineManagerService.java
+++ src/main/java/com/redhat/vertx/search/PipelineManagerService.java
@@ -1,25 +1,44 @@
 package com.redhat.vertx.search;
 
-import javax.enterprise.context.ApplicationScoped;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import com.redhat.vertx.pool.EnginePool;
+import io.reactivex.Single;
+import io.vertx.reactivex.core.Vertx;
 
 @ApplicationScoped
 public class PipelineManagerService {
-    private Map<String,String> filesystem = new HashMap<>();
-    private EnginePool enginePool;
+    Map<String,String> filesystem = new HashMap<>();
+    EnginePool enginePool;
 
-    public CompletionStage<String> get(String name) {
-        return CompletableFuture.supplyAsync(() -> filesystem.get(name));
+    @Inject
+    Vertx vertx;
+
+    @PostConstruct
+    public void init() {
+//        enginePool = new EnginePool(); // TODO need to figure out the best approach for using the engine
     }
 
-    public CompletionStage<String> set(String name, String pipeline) {
-        return CompletableFuture.supplyAsync(() -> filesystem.put(name, pipeline));
+    public Single<String> get(String name) {
+        return Single.create(emitter -> {
+            vertx.fileSystem().rxReadFile(name).subscribe(
+                    buffer -> emitter.onSuccess(buffer.toString()),
+                    emitter::tryOnError
+            );
+        });
     }
 
-    public CompletionStage<String> run(String pipeline, String doc) {
+    public Single<String> set(String name, String pipeline) {
+        return null;
+    }
+
+    public Single<String> run(String pipeline, String doc) {
+        return null;
     }
 
 }
