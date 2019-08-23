83fa9aae086570e8f8ba14bc1f243381669f7a33
Jim Scarborough <jscarbor@redhat.com>
2019-08-23 16:32:24 -0400

    Solr client, properties
    Squashed commit of the following:
    
    commit 07426c42a258b4100c1578fe855fb1d41b6b6f36
    Author: Jim Scarborough <jscarbor@redhat.com>
    Date:   Fri Aug 23 16:31:20 2019 -0400
    
        Setting up environment for specification of Solr host
    
    commit c7d22d8c156c63a13a9068854869ba704a7330df
    Author: Jason Porter <lightguard.jp@gmail.com>
    Date:   Wed Aug 21 16:45:14 2019 -0600
    
        Playing around with Service Loader
    
        Things seem to be working!
    
    commit 57ab6918c32d466056476f589275d0135f134c09
    Author: Jim Scarborough <jscarbor@redhat.com>
    Date:   Wed Aug 21 15:17:37 2019 -0400
    
        WIP SolrClient step
    
    commit 7c55acf10ea1c7922cb040e723737a5fde75e64d
    Author: Jim Scarborough <jscarbor@redhat.com>
    Date:   Mon Aug 19 11:16:01 2019 -0400
    
        Hello World works

--- src/main/java/com/redhat/vertx/search/PipelineManagerService.java
+++ src/main/java/com/redhat/vertx/search/PipelineManagerService.java
@@ -1,27 +1,31 @@
 package com.redhat.vertx.search;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import com.redhat.vertx.Engine;
 import com.redhat.vertx.pool.EnginePool;
 import io.reactivex.Single;
+import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.Vertx;
+import org.eclipse.microprofile.config.Config;
 
 @ApplicationScoped
 public class PipelineManagerService {
-    Map<String,String> filesystem = new HashMap<>();
-    EnginePool enginePool;
+    private EnginePool enginePool;
+
+
+    @Inject
+    Config config;
 
     @Inject
     Vertx vertx;
 
     @PostConstruct
     public void init() {
-//        enginePool = new EnginePool(); // TODO need to figure out the best approach for using the engine
+        getEnginePool();
     }
 
     public Single<String> get(String name) {
@@ -37,8 +41,22 @@ public class PipelineManagerService {
         return null;
     }
 
+
     public Single<String> run(String pipeline, String doc) {
-        return null;
+        return Single.create(ran -> {
+            Single<Engine> engineSingle = getEnginePool().getEngineByPipelineName(pipeline);
+            engineSingle.subscribe(
+                    engine -> engine.execute(new JsonObject(doc)).subscribe(d -> ran.onSuccess(d.toString())),
+                    ran::onError
+            );
+        });
+    }
+
+    private EnginePool getEnginePool() {
+        if (enginePool == null) {
+            enginePool = new EnginePool(this::get,vertx, new ConfigMap(config));
+        }
+        return enginePool;
     }
 
 }
---
+++ src/main/java/com/redhat/vertx/search/step/SolrClient.java
@@ -0,0 +1,96 @@
+package com.redhat.vertx.search.step;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.redhat.vertx.Engine;
+import com.redhat.vertx.pipeline.AbstractStep;
+import com.redhat.vertx.pipeline.Step;
+import com.redhat.vertx.pipeline.StepDependencyNotMetException;
+import io.reactivex.Maybe;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.ext.web.client.WebClient;
+import io.vertx.reactivex.ext.web.codec.BodyCodec;
+import org.apache.http.client.utils.URIBuilder;
+import org.jsoup.HttpStatusException;
+import org.kohsuke.MetaInfServices;
+
+@MetaInfServices(Step.class)
+public class SolrClient extends AbstractStep {
+    private static final Set<String> PARAMS = new HashSet<>(Arrays.asList((
+            "sow,mm.autoRelax,boost,lowercaseOperators,ps,pf2,ps2,pf3,ps3,stopwords,uf,qf," +
+                    "defType,sort,start,rows,fq,fl,debug,explainOther,timeAllowed,segmentTerminateEarly," +
+                    "omitHeader,wt,cache,logParamsList,echoParams,q").split("\\,")));
+    private WebClient http;
+
+    @Override
+    public void init(Engine engine, JsonObject config) {
+        super.init(engine, config);
+    }
+
+    @Override
+    protected Maybe<Object> executeSlow(JsonObject env) {
+        if (http == null) {
+            http = WebClient.create(engine.getRxVertx());
+        }
+
+        String host_url = env.getString("host_url");
+        String path = env.getString("path");
+        String q = env.getString("q");
+
+        if (host_url == null || path == null || q == null || host_url.length()<1 || path.length() <1 || q.length() <1 ) {
+            return Maybe.error(new StepDependencyNotMetException());
+        }
+
+        URIBuilder b = null;
+        final URL url;
+        try {
+            b = new URIBuilder(host_url);
+            b.setPath(path);
+            for (String param: PARAMS.stream().filter(env::containsKey).collect(Collectors.toSet())) {
+                Object p = env.getValue(param);
+                if (p instanceof JsonArray) {
+                    for (Object pp : ((JsonArray)p)) {
+                        b.addParameter(param,(String)pp);
+                    }
+                } else {
+                    b.addParameter(param,(String)p);
+                }
+            }
+            b.setParameter("wt","json");
+            b.setParameter("indent","on");
+            url = b.build().toURL();
+        } catch (URISyntaxException | MalformedURLException e) {
+            return Maybe.error(e);
+        }
+
+        logger.info(url.toString());
+
+        return Maybe.create( step ->
+                addDisposable(env.getJsonObject("doc").getString(Engine.DOC_UUID),
+                        http.getAbs(url.toString())
+                                .putHeader("Content-type","application/json")
+                                .as(BodyCodec.jsonObject())
+                                .rxSend()
+                                .subscribe(response -> {
+                                            if (response.statusCode() == 200) {
+                                                step.onSuccess(response.body());
+                                            } else {
+                                                // TODO make a new exception - this is a weird dependency
+                                                step.onError(new HttpStatusException(response.bodyAsString(),response.statusCode(),url.toString()));
+                                            }
+
+                                        },
+                                        err -> {
+                                            step.onError(err);
+                                        })
+                )
+        );
+    }
+}
