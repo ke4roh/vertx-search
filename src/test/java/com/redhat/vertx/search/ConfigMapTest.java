cf4fcb2615100dc94cf0cb845f4afa51d3bbd172
Jim Scarborough <jscarbor@redhat.com>
2019-08-26 14:28:04 -0400

    ConfigMap cleanup

--- src/test/java/com/redhat/vertx/search/ConfigMapTest.java
+++ src/test/java/com/redhat/vertx/search/ConfigMapTest.java
@@ -13,6 +13,20 @@ public class ConfigMapTest {
     public void testConfigMap() {
         Config config = ConfigProvider.getConfig();
         Map<String, String> map = new ConfigMap(config);
+
+        // Something in the map
         assertThat(map.get("file.separator")).isEqualTo(System.getProperties().get("file.separator"));
+        assertThat(map.containsKey("file.separator")).isTrue();
+
+        // Something not in the map
+        assertThat(map.containsKey("supercalifragilisticexpealidocious")).isFalse();
+        assertThat(map.get("supercalifragilisticexpealidocious")).isNull();
+
+        // <code>this</code> certainly is not in the map, firstly because it is not a String
+        assertThat(map.containsKey(this)).isFalse();
+        assertThat(map.get(this)).isNull();
+
+        // These two probably do the same thing, but they force iteration over the entry set.
+        assertThat(map.entrySet().size()).isEqualTo(map.size());
     }
 }
