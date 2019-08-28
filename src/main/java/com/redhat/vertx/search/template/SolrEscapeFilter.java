57f3b212fc92515c7cd7f1084fe0dc71302009dd
Jason Porter <lightguard.jp@gmail.com>
2019-08-28 11:21:12 -0600

    Fixes #6 - solr_escape implemented
    I couldn't do this with a straight regex through jinjava, the magic
    incantation escaped (yep, I did that) me after hours of trying.

---
+++ src/main/java/com/redhat/vertx/search/template/SolrEscapeFilter.java
@@ -0,0 +1,36 @@
+package com.redhat.vertx.search.template;
+
+import java.util.Objects;
+
+import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+import com.hubspot.jinjava.lib.filter.Filter;
+import org.kohsuke.MetaInfServices;
+
+/**
+ * Jinjava filter to properly escape something before sending it to solr.
+ *
+ * The string passed in will search for the following characters: :+!^"~*?|\/()[]{}
+ * Those characters are then each escaped with a '\'.
+ *
+ * Usage: {{ var | solr_escape }}
+ * var = [Sphinx] :of (black) +{quartz}^, " * ? ~ + | / \ judge my & vow!
+ * Result: \[Sphinx\] \:of \(black\) \+\{quartz\}\^, \" \* \? \~ \+ \| \/ \\ judge my & vow\!
+ */
+@MetaInfServices
+public class SolrEscapeFilter implements Filter {
+    @Override
+    public Object filter(Object var, JinjavaInterpreter interpreter, String... args) {
+        if (Objects.isNull(var) || var.toString().isBlank()) {
+            return "";
+        }
+        var pattern = "([\\Q:+!^\"~*?|\\/()[]{}\\E])";
+        final String replacement = "\\\\$1";
+
+        return var.toString().replaceAll(pattern, replacement);
+    }
+
+    @Override
+    public String getName() {
+        return "solr_escape";
+    }
+}
